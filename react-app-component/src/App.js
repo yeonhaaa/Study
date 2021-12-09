4b6956c8ec0a4f7620dbcac3cfe3b70dfc75e43f
yeonhaaa <qkdxo101@naver.com>
2021-12-09 17:54:22 +0900

    최초 업로드

---
+++ react-app-component/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component, useState } from 'react';
+
+class ClassComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initNumber : this.props.initNumber
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>Class Style Component</h2>
+        <p>Number : {this.props.initNumber}</p>
+      </div>
+    )
+  }
+}
+function FunctionComp(props) {
+
+  const num = useState(props.initNumber);
+
+  return (
+    <div className="container">
+      <h2>Function Style Component</h2>
+      <p>Number : {num[0]}</p>
+    </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div className="container"> 
+      <h1>Hello World</h1>
+      <ClassComp initNumber={2}></ClassComp>
+      <FunctionComp initNumber={2}></FunctionComp>
+    </div>
+  );
+}
+export default App;
+
